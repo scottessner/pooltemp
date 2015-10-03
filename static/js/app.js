3813cad35008d26c694d9d7e4a37c8c7c006d38f
scottessner <scott.essner@gmail.com>
2015-10-03 01:15:37 -0500

    push first measurement info

--- static/js/app.js
+++ static/js/app.js
@@ -4,7 +4,6 @@
 var app = angular.module('app', ['tc.chartjs', 'ui.bootstrap']);
 
 app.controller('MeasController', function($http){
-   var app = this;
 
     $http.get('/api/meas').success(function(data){
         this.meas = data.objects;
