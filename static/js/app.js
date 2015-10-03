132dffd565d92bf0e3b181257406e5c6be718dbf
scottessner <scott.essner@gmail.com>
2015-10-03 08:34:58 -0500

    debugging

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,16 @@
 /**
  * Created by ssessner on 9/23/2015.
  */
+(function(){
 var app = angular.module('app', ['tc.chartjs', 'ui.bootstrap']);
 
-app.controller('MeasController', function($http){
-
-    $http.get('/api/meas').success(function(data){
-        this.meas = data.objects;
-    })
+    app.controller('MeasController', function($http) {
+        this.myvar = 'Hi World!';
 
 
+        $http.get('/api/meas').success(function (data) {
+            this.meas = data.objects;
+        });
+    });
 
-});
+})();
