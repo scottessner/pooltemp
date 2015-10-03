8aea82f689ddc8bce288ccbca1b3af664b37b6ce
scottessner <scott.essner@gmail.com>
2015-10-03 08:59:35 -0500

    Attempting to capture current reading

--- static/js/app.js
+++ static/js/app.js
@@ -4,13 +4,14 @@
 (function(){
 var app = angular.module('app', ['tc.chartjs', 'ui.bootstrap']);
 
-    app.controller('MeasController', function($http) {
+    app.controller('MeasController', function($http, $filter) {
         var app = this;
         app.myvar = 'Hi World!';
 
 
         $http.get('/api/meas').success(function (data) {
             app.meas = data.objects;
+            app.current = $filter('orderBy')(app.meas,'-local_epoch')[this.data.length-1].local_epoch ;
         });
     });
 
