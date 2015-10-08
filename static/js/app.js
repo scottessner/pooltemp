53c568213a01b0788bea2c61cf956003f2ba2638
scottessner <scott.essner@gmail.com>
2015-10-08 12:34:17 -0500

    Split Table to its own directive.  Revised Main controller formatting.

--- static/js/app.js
+++ static/js/app.js
@@ -1,23 +1,27 @@
 /**
  * Created by ssessner on 9/23/2015.
  */
-(function(){
-var app = angular.module('app', ['tc.chartjs', 'ui.bootstrap']);
+(function() {
+    var app = angular.module('app', ['tc.chartjs', 'ui.bootstrap']);
 
-    app.controller('MeasController', ['$scope', '$filter', '$http', function($scope, $filter, $http) {
-
-        var orderBy = $filter('orderBy');
+    app.controller('MeasController', ['$http', function ($http) {
+        var mctl = this;
+        mctl.readings = [];
 
         $http.get('/api/meas').success(function (data) {
-            $scope.meas = data.objects;
-            $scope.meas = orderBy($scope.meas, '-local_epoch');
-            $scope.latest = $scope.meas[0];
-            //app.current = $filter('orderBy')(app.meas,'-local_epoch')[this.data.length-1].local_epoch ;
+            mctl.readings = data.objects;
         });
 
-        //$scope.latest = $scope.meas[0];
-
+        mctl.latest = mctl.readings[0];
 
     }]);
+
+    app.directive('measTable', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'elements/meas-table.html'
+
+        }
+     });
 
 })();
