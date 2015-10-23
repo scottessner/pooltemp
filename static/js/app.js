a01b81852f2250deec3dc6955632ed3f2deebb92
scottessner <scott.essner@gmail.com>
2015-10-23 13:23:11 -0500

    Loading only current reading on page load.

--- static/js/app.js
+++ static/js/app.js
@@ -2,26 +2,38 @@
  * Created by ssessner on 9/23/2015.
  */
 (function() {
-    var app = angular.module('app', ['tc.chartjs', 'ngMaterial'])
+    var app = angular.module('app', ['tc.chartjs', 'ngMaterial']);
+
+    app.controller('CurrentController', ['$http', '$scope', function($http, $scope) {
+
+        var order_param = {"q":{"order_by":[{"field": "local_epoch", "direction": "desc"}],"limit": 1}};
+
+        $http.get('/api/meas', {params: order_param}).success(function (data) {
+            $scope.current = data;
+            //$scope.latest = $scope.readings[0];
+        });
+    }]);
 
     app.controller('MeasController', ['$http', '$scope', function ($http, $scope) {
         var mctl = this;
         $scope.readings = [];
 
-        var order_param = {"q":{"order_by":[ {"field": "local_epoch", "direction": "desc"}]}};
+        var current_param = {"q":{"order_by":[{"field": "local_epoch", "direction": "desc"}],"limit": 1}};
 
-        $http.get('/api/meas', {params: order_param}).success(function (data) {
-            $scope.readings = data.objects;
-            $scope.latest = $scope.readings[0];
-        });
+        var history_param = {"q":{"order_by":[ {"field": "local_epoch", "direction": "desc"}]}};
 
-        $scope.shown = 0;
+        $scope.load = function(params){
 
-        this.showTable = function(){
-            if($scope.shown)
-                $scope.shown = 0;
-            else
-                $scope.shown = 1;
+            $http.get('/api/meas', {params: params}).success(function (data) {
+                $scope.readings = data.objects;
+                $scope.current = $scope.readings[0];
+            });
+        };
+
+        $scope.load(current_param);
+
+        $scope.loadHistory = function(){
+            $scope.load(history_param);
         };
 
     }]);
